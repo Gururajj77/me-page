8a70698dffe5b9f4a87ad0c3be813a7caeb2bdd3
Gururajj77 <gururajtakeshi@gmail.com>
2023-11-26 19:06:35 +0530

    test commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inconsolata", "monospace"],
+      },
+      colors: {
+        "default-text": "#2F3C7E",
+        "default-bg": "#FBEAEB",
+      },
+    },
+  },
+  plugins: [],
+};
